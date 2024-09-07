a7e288d7f04c241bcd8e5b5c58b390ff0207d461
Zane Shannon <z@zcs.me>
2024-09-07 14:44:54 -0700

    imported ONNX runtime

--- Package.swift
+++ Package.swift
@@ -5,19 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSileroVAD",
+    platforms: [.iOS(.v12), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftSileroVAD",
-            targets: ["SwiftSileroVAD"]),
+            targets: ["SwiftSileroVAD"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/microsoft/onnxruntime-swift-package-manager",
+            branch: "main"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftSileroVAD"),
+        .target(name: "SwiftSileroVAD",
+                dependencies: [
+                    .product(name: "onnxruntime", package: "onnxruntime-swift-package-manager"),
+                ]),
         .testTarget(
             name: "SwiftSileroVADTests",
-            dependencies: ["SwiftSileroVAD"]),
+            dependencies: ["SwiftSileroVAD"]
+        ),
     ]
 )
