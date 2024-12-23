711adffa0272f4df78f7e8e338c2d37f6b43e6b9
Zane Shannon <z@zcs.me>
2024-12-22 21:53:13 -0800

    create utils

---
+++ Sources/SwiftSileroVAD/Utils.swift
@@ -0,0 +1,31 @@
+//
+//  Utils.swift
+//  SwiftSileroVAD
+//
+//  Created by Zane Shannon on 12/22/24.
+//
+
+import AVFoundation
+
+func readAudioFile(url: URL) throws -> [Float] {
+    enum Error: Swift.Error, Codable, Sendable { case failedToOpenFile, failedToCreatePCMBuffer, failedToReadFile }
+    guard let file = try? AVAudioFile(forReading: url) else {
+        throw Error.failedToOpenFile
+    }
+    let format = AVAudioFormat(
+        commonFormat: .pcmFormatFloat32,
+        sampleRate: file.fileFormat.sampleRate,
+        channels: 1,
+        interleaved: false
+    )
+    guard let buffer = AVAudioPCMBuffer(pcmFormat: format!, frameCapacity: AVAudioFrameCount(file.length)) else {
+        throw Error.failedToCreatePCMBuffer
+    }
+    do {
+        try file.read(into: buffer)
+    } catch {
+        throw Error.failedToReadFile
+    }
+    let floatArray = Array(UnsafeBufferPointer(start: buffer.floatChannelData?[0], count: Int(buffer.frameLength)))
+    return floatArray
+}
--- Tests/SwiftSileroVADTests/SwiftSileroVADTests.swift
+++ Tests/SwiftSileroVADTests/SwiftSileroVADTests.swift
@@ -1,4 +1,3 @@
-import AVFoundation
 @testable import SwiftSileroVAD
 import XCTest
 
@@ -23,28 +22,5 @@ final class SwiftSileroVADTests: XCTestCase {
         XCTAssertEqual(probabilities[200], 0.91776323)
         XCTAssertEqual(probabilities[300], 0.6470929)
         XCTAssertEqual(probabilities[343], 0.57817054)
-    }
-
-    private func readAudioFile(url: URL) throws -> [Float] {
-        enum Error: Swift.Error, Codable, Sendable { case failedToOpenFile, failedToCreatePCMBuffer, failedToReadFile }
-        guard let file = try? AVAudioFile(forReading: url) else {
-            throw Error.failedToOpenFile
-        }
-        let format = AVAudioFormat(
-            commonFormat: .pcmFormatFloat32,
-            sampleRate: file.fileFormat.sampleRate,
-            channels: 1,
-            interleaved: false
-        )
-        guard let buffer = AVAudioPCMBuffer(pcmFormat: format!, frameCapacity: AVAudioFrameCount(file.length)) else {
-            throw Error.failedToCreatePCMBuffer
-        }
-        do {
-            try file.read(into: buffer)
-        } catch {
-            throw Error.failedToReadFile
-        }
-        let floatArray = Array(UnsafeBufferPointer(start: buffer.floatChannelData?[0], count: Int(buffer.frameLength)))
-        return floatArray
     }
 }
